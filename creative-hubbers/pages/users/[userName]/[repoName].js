52509459ad1d7df7e008e138896006538f6c040c
Diana Caraian <diana.caraian@evozon.com>
2021-08-24 11:50:30 +0300

    Update repo page

--- creative-hubbers/pages/users/[userName]/[repoName].js
+++ creative-hubbers/pages/users/[userName]/[repoName].js
@@ -7,24 +7,7 @@ import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import { Typography } from "@material-ui/core";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  },
-  media: {
-    height: 300,
-  },
-  cards: {
-    maxWidth: 450,
-  },
-  typography: {},
-}));
+import FolderIcon from "@material-ui/icons/Folder";
 
 const repoName = () => {
   return (
@@ -33,16 +16,25 @@ const repoName = () => {
 
       <div className={styles.content}>
         <h3 className={styles.foldername}>File Name</h3>
-        <div className={styles.filename}>
-          <Typography gutterBottom variant="h4" component="h4">
-            File Name
-          </Typography>
-          <Typography gutterBottom variant="h4" component="h4">
-            File Name
-          </Typography>
-          <Typography gutterBottom variant="h4" component="h4">
-            File Name
-          </Typography>
+        <div className={styles.fileContainer}>
+          <div className={styles.fileElem}>
+            <FolderIcon className={styles.icon} />
+            <Typography gutterBottom variant="h4" component="h4">
+              File Name
+            </Typography>
+          </div>
+          <div className={styles.fileElem}>
+            <FolderIcon className={styles.icon} />
+            <Typography gutterBottom variant="h4" component="h4">
+              File Name
+            </Typography>
+          </div>
+          <div className={styles.fileElem}>
+            <FolderIcon className={styles.icon} />
+            <Typography gutterBottom variant="h4" component="h4">
+              File Name
+            </Typography>
+          </div>
         </div>
       </div>
     </div>
