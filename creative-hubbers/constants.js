a3c047df31be0868d29573128f5214bbb7f6ba40
Diana Caraian <diana.caraian@evozon.com>
2021-08-25 13:34:53 +0300

    Added fetch on click

---
+++ creative-hubbers/constants.js
@@ -0,0 +1,233 @@
+export const repos = [
+  {
+    name: ".gitignore",
+    path: ".gitignore",
+    sha: "c06a73d628316ba21fd2f1fe0a26933f4b118462",
+    size: 14,
+    url: "https://api.github.com/repos/mojombo/grit/contents/.gitignore?ref=master",
+    html_url: "https://github.com/mojombo/grit/blob/master/.gitignore",
+    git_url:
+      "https://api.github.com/repos/mojombo/grit/git/blobs/c06a73d628316ba21fd2f1fe0a26933f4b118462",
+    download_url:
+      "https://raw.githubusercontent.com/mojombo/grit/master/.gitignore",
+    type: "file",
+    _links: {
+      self: "https://api.github.com/repos/mojombo/grit/contents/.gitignore?ref=master",
+      git: "https://api.github.com/repos/mojombo/grit/git/blobs/c06a73d628316ba21fd2f1fe0a26933f4b118462",
+      html: "https://github.com/mojombo/grit/blob/master/.gitignore",
+    },
+  },
+  {
+    name: "API.txt",
+    path: "API.txt",
+    sha: "01dfb7bd1ff02e3eeb76f0cdd584f281e905adc5",
+    size: 2366,
+    url: "https://api.github.com/repos/mojombo/grit/contents/API.txt?ref=master",
+    html_url: "https://github.com/mojombo/grit/blob/master/API.txt",
+    git_url:
+      "https://api.github.com/repos/mojombo/grit/git/blobs/01dfb7bd1ff02e3eeb76f0cdd584f281e905adc5",
+    download_url:
+      "https://raw.githubusercontent.com/mojombo/grit/master/API.txt",
+    type: "file",
+    _links: {
+      self: "https://api.github.com/repos/mojombo/grit/contents/API.txt?ref=master",
+      git: "https://api.github.com/repos/mojombo/grit/git/blobs/01dfb7bd1ff02e3eeb76f0cdd584f281e905adc5",
+      html: "https://github.com/mojombo/grit/blob/master/API.txt",
+    },
+  },
+  {
+    name: "History.txt",
+    path: "History.txt",
+    sha: "97d15e99bb65b51e83cd5106f5c16e4430113773",
+    size: 6328,
+    url: "https://api.github.com/repos/mojombo/grit/contents/History.txt?ref=master",
+    html_url: "https://github.com/mojombo/grit/blob/master/History.txt",
+    git_url:
+      "https://api.github.com/repos/mojombo/grit/git/blobs/97d15e99bb65b51e83cd5106f5c16e4430113773",
+    download_url:
+      "https://raw.githubusercontent.com/mojombo/grit/master/History.txt",
+    type: "file",
+    _links: {
+      self: "https://api.github.com/repos/mojombo/grit/contents/History.txt?ref=master",
+      git: "https://api.github.com/repos/mojombo/grit/git/blobs/97d15e99bb65b51e83cd5106f5c16e4430113773",
+      html: "https://github.com/mojombo/grit/blob/master/History.txt",
+    },
+  },
+  {
+    name: "LICENSE",
+    path: "LICENSE",
+    sha: "a4ed2f5bd6c477998b2eead149df1a84dccba52f",
+    size: 1085,
+    url: "https://api.github.com/repos/mojombo/grit/contents/LICENSE?ref=master",
+    html_url: "https://github.com/mojombo/grit/blob/master/LICENSE",
+    git_url:
+      "https://api.github.com/repos/mojombo/grit/git/blobs/a4ed2f5bd6c477998b2eead149df1a84dccba52f",
+    download_url:
+      "https://raw.githubusercontent.com/mojombo/grit/master/LICENSE",
+    type: "file",
+    _links: {
+      self: "https://api.github.com/repos/mojombo/grit/contents/LICENSE?ref=master",
+      git: "https://api.github.com/repos/mojombo/grit/git/blobs/a4ed2f5bd6c477998b2eead149df1a84dccba52f",
+      html: "https://github.com/mojombo/grit/blob/master/LICENSE",
+    },
+  },
+  {
+    name: "PURE_TODO",
+    path: "PURE_TODO",
+    sha: "dbf2b82b845b14015fa545f7c192828f9f0c1a6a",
+    size: 1556,
+    url: "https://api.github.com/repos/mojombo/grit/contents/PURE_TODO?ref=master",
+    html_url: "https://github.com/mojombo/grit/blob/master/PURE_TODO",
+    git_url:
+      "https://api.github.com/repos/mojombo/grit/git/blobs/dbf2b82b845b14015fa545f7c192828f9f0c1a6a",
+    download_url:
+      "https://raw.githubusercontent.com/mojombo/grit/master/PURE_TODO",
+    type: "file",
+    _links: {
+      self: "https://api.github.com/repos/mojombo/grit/contents/PURE_TODO?ref=master",
+      git: "https://api.github.com/repos/mojombo/grit/git/blobs/dbf2b82b845b14015fa545f7c192828f9f0c1a6a",
+      html: "https://github.com/mojombo/grit/blob/master/PURE_TODO",
+    },
+  },
+  {
+    name: "README.md",
+    path: "README.md",
+    sha: "a821222a46d3cb4e46bef88a89fa1c3a15da626d",
+    size: 6641,
+    url: "https://api.github.com/repos/mojombo/grit/contents/README.md?ref=master",
+    html_url: "https://github.com/mojombo/grit/blob/master/README.md",
+    git_url:
+      "https://api.github.com/repos/mojombo/grit/git/blobs/a821222a46d3cb4e46bef88a89fa1c3a15da626d",
+    download_url:
+      "https://raw.githubusercontent.com/mojombo/grit/master/README.md",
+    type: "file",
+    _links: {
+      self: "https://api.github.com/repos/mojombo/grit/contents/README.md?ref=master",
+      git: "https://api.github.com/repos/mojombo/grit/git/blobs/a821222a46d3cb4e46bef88a89fa1c3a15da626d",
+      html: "https://github.com/mojombo/grit/blob/master/README.md",
+    },
+  },
+  {
+    name: "Rakefile",
+    path: "Rakefile",
+    sha: "be391a3318952a73eaad63fd988ab1e201386965",
+    size: 3722,
+    url: "https://api.github.com/repos/mojombo/grit/contents/Rakefile?ref=master",
+    html_url: "https://github.com/mojombo/grit/blob/master/Rakefile",
+    git_url:
+      "https://api.github.com/repos/mojombo/grit/git/blobs/be391a3318952a73eaad63fd988ab1e201386965",
+    download_url:
+      "https://raw.githubusercontent.com/mojombo/grit/master/Rakefile",
+    type: "file",
+    _links: {
+      self: "https://api.github.com/repos/mojombo/grit/contents/Rakefile?ref=master",
+      git: "https://api.github.com/repos/mojombo/grit/git/blobs/be391a3318952a73eaad63fd988ab1e201386965",
+      html: "https://github.com/mojombo/grit/blob/master/Rakefile",
+    },
+  },
+  {
+    name: "benchmarks.rb",
+    path: "benchmarks.rb",
+    sha: "e445e286614effdcb418b971d2ee8545cc507c24",
+    size: 2835,
+    url: "https://api.github.com/repos/mojombo/grit/contents/benchmarks.rb?ref=master",
+    html_url: "https://github.com/mojombo/grit/blob/master/benchmarks.rb",
+    git_url:
+      "https://api.github.com/repos/mojombo/grit/git/blobs/e445e286614effdcb418b971d2ee8545cc507c24",
+    download_url:
+      "https://raw.githubusercontent.com/mojombo/grit/master/benchmarks.rb",
+    type: "file",
+    _links: {
+      self: "https://api.github.com/repos/mojombo/grit/contents/benchmarks.rb?ref=master",
+      git: "https://api.github.com/repos/mojombo/grit/git/blobs/e445e286614effdcb418b971d2ee8545cc507c24",
+      html: "https://github.com/mojombo/grit/blob/master/benchmarks.rb",
+    },
+  },
+  {
+    name: "benchmarks.txt",
+    path: "benchmarks.txt",
+    sha: "f9d9a7b2ceb685021adb52830e8f1360f2e805da",
+    size: 952,
+    url: "https://api.github.com/repos/mojombo/grit/contents/benchmarks.txt?ref=master",
+    html_url: "https://github.com/mojombo/grit/blob/master/benchmarks.txt",
+    git_url:
+      "https://api.github.com/repos/mojombo/grit/git/blobs/f9d9a7b2ceb685021adb52830e8f1360f2e805da",
+    download_url:
+      "https://raw.githubusercontent.com/mojombo/grit/master/benchmarks.txt",
+    type: "file",
+    _links: {
+      self: "https://api.github.com/repos/mojombo/grit/contents/benchmarks.txt?ref=master",
+      git: "https://api.github.com/repos/mojombo/grit/git/blobs/f9d9a7b2ceb685021adb52830e8f1360f2e805da",
+      html: "https://github.com/mojombo/grit/blob/master/benchmarks.txt",
+    },
+  },
+  {
+    name: "examples",
+    path: "examples",
+    sha: "04a120c4a3754b146917deeab1b1ba5dcbe4b6a2",
+    size: 0,
+    url: "https://api.github.com/repos/mojombo/grit/contents/examples?ref=master",
+    html_url: "https://github.com/mojombo/grit/tree/master/examples",
+    git_url:
+      "https://api.github.com/repos/mojombo/grit/git/trees/04a120c4a3754b146917deeab1b1ba5dcbe4b6a2",
+    download_url: null,
+    type: "dir",
+    _links: {
+      self: "https://api.github.com/repos/mojombo/grit/contents/examples?ref=master",
+      git: "https://api.github.com/repos/mojombo/grit/git/trees/04a120c4a3754b146917deeab1b1ba5dcbe4b6a2",
+      html: "https://github.com/mojombo/grit/tree/master/examples",
+    },
+  },
+  {
+    name: "grit.gemspec",
+    path: "grit.gemspec",
+    sha: "dee1a45bf74c1cf509ba24372686a3814fb124c5",
+    size: 2018,
+    url: "https://api.github.com/repos/mojombo/grit/contents/grit.gemspec?ref=master",
+    html_url: "https://github.com/mojombo/grit/blob/master/grit.gemspec",
+    git_url:
+      "https://api.github.com/repos/mojombo/grit/git/blobs/dee1a45bf74c1cf509ba24372686a3814fb124c5",
+    download_url:
+      "https://raw.githubusercontent.com/mojombo/grit/master/grit.gemspec",
+    type: "file",
+    _links: {
+      self: "https://api.github.com/repos/mojombo/grit/contents/grit.gemspec?ref=master",
+      git: "https://api.github.com/repos/mojombo/grit/git/blobs/dee1a45bf74c1cf509ba24372686a3814fb124c5",
+      html: "https://github.com/mojombo/grit/blob/master/grit.gemspec",
+    },
+  },
+  {
+    name: "lib",
+    path: "lib",
+    sha: "518b4fe02bbf82a84b8514df4a73f8db4dfaa616",
+    size: 0,
+    url: "https://api.github.com/repos/mojombo/grit/contents/lib?ref=master",
+    html_url: "https://github.com/mojombo/grit/tree/master/lib",
+    git_url:
+      "https://api.github.com/repos/mojombo/grit/git/trees/518b4fe02bbf82a84b8514df4a73f8db4dfaa616",
+    download_url: null,
+    type: "dir",
+    _links: {
+      self: "https://api.github.com/repos/mojombo/grit/contents/lib?ref=master",
+      git: "https://api.github.com/repos/mojombo/grit/git/trees/518b4fe02bbf82a84b8514df4a73f8db4dfaa616",
+      html: "https://github.com/mojombo/grit/tree/master/lib",
+    },
+  },
+  {
+    name: "test",
+    path: "test",
+    sha: "21a4a6e67a713461d278735c964ac48cd5723ebe",
+    size: 0,
+    url: "https://api.github.com/repos/mojombo/grit/contents/test?ref=master",
+    html_url: "https://github.com/mojombo/grit/tree/master/test",
+    git_url:
+      "https://api.github.com/repos/mojombo/grit/git/trees/21a4a6e67a713461d278735c964ac48cd5723ebe",
+    download_url: null,
+    type: "dir",
+    _links: {
+      self: "https://api.github.com/repos/mojombo/grit/contents/test?ref=master",
+      git: "https://api.github.com/repos/mojombo/grit/git/trees/21a4a6e67a713461d278735c964ac48cd5723ebe",
+      html: "https://github.com/mojombo/grit/tree/master/test",
+    },
+  },
+];
