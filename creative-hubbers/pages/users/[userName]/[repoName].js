1536bdadc644a8028582745e4cbe1dec690a989c
Nora Leonte <nora.leonte@evozon.com>
2021-08-25 16:26:51 +0300

    showing repo content

--- creative-hubbers/pages/users/[userName]/[repoName].js
+++ creative-hubbers/pages/users/[userName]/[repoName].js
@@ -1,30 +1,30 @@
-import React, { useState, useEffect } from "react";
-import styles from "../../../styles/Repo.module.css";
-import { CardContent, Typography } from "@material-ui/core";
-import FolderIcon from "@material-ui/icons/Folder";
-import InsertDriveFileOutlinedIcon from "@material-ui/icons/InsertDriveFileOutlined";
-import { makeStyles } from "@material-ui/core/styles";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import { useRouter } from "next/router";
-import { useDispatch, useSelector } from "react-redux";
-import { getRepos } from "../../../actions";
+import React, { useState, useEffect } from 'react';
+import styles from '../../../styles/Repo.module.css';
+import { CardContent, Typography } from '@material-ui/core';
+import FolderIcon from '@material-ui/icons/Folder';
+import InsertDriveFileOutlinedIcon from '@material-ui/icons/InsertDriveFileOutlined';
+import { makeStyles } from '@material-ui/core/styles';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import { useRouter } from 'next/router';
+import { useDispatch, useSelector } from 'react-redux';
+import { getRepos } from '../../../actions';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: "flex",
-    padding: "30px",
+    display: 'flex',
+    padding: '30px',
   },
   details: {
-    display: "flex",
-    flexDirection: "column",
-    padding: "30px",
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '30px',
   },
   content: {
-    flex: "1 0 auto",
+    flex: '1 0 auto',
   },
   cover: {
-    width: "25%",
-    height: "100%",
+    width: '25%',
+    height: '100%',
   },
 }));
 
@@ -32,72 +32,87 @@ const repoName = () => {
   const classes = useStyles();
 
   const router = useRouter();
+  const dispatch = useDispatch(); //Declanseaza actiunea
 
   const { userName } = router.query;
   const { repoName } = router.query;
 
-  console.log("userName: ", userName);
-  console.log("repoName: ", repoName);
+  console.log('userName: ', userName);
+  console.log('repoName: ', repoName);
 
-  const dispatch = useDispatch(); //Declanseaza actiunea
+  const fetchRepo = (additionalDirs = '') => {
+    if (userName && repoName) {
+      let dir;
 
-  // const fetchRepo = () => {
-  //   let url =
-  //     "https://api.github.com/repos/" + userName + "/" + repoName + "/contents";
-  //   (async () => {
-  //     const res = await fetch(url);
-  //     const data = await res.json();
+      if (additionalDirs) {
+        dir = '/' + additionalDirs;
+      } else {
+        dir = '';
+      }
+      let url =
+        'https://api.github.com/repos/' +
+        userName +
+        '/' +
+        repoName +
+        '/contents' +
+        dir;
+      (async () => {
+        const res = await fetch(url);
+        const data = await res.json();
 
-  //     console.log("data: ", data);
-  //     console.log("url: ", url);
+        console.log('data: ', data);
+        console.log('url: ', url);
 
-  //     dispatch(getRepos(data));
-  //   })();
-  // };
+        dispatch(getRepos(data));
+      })();
+    }
+  };
+  useEffect(fetchRepo, [router.isReady]);
 
-  // fetchRepo();
-
+  // fetchRepo(userName, repoName);
   const repos = useSelector((state) => state.user.repos);
 
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>Repository</h2>
 
-      <button
+      {/* <button
         onClick={() => {
           let url =
-            "https://api.github.com/repos/" +
+            'https://api.github.com/repos/' +
             userName +
-            "/" +
+            '/' +
             repoName +
-            "/contents";
+            '/contents';
           (async () => {
             const res = await fetch(url);
             const data = await res.json();
 
-            console.log("data: ", data);
-            console.log("url: ", url);
+            console.log('data: ', data);
+            console.log('url: ', url);
 
             dispatch(getRepos(data));
           })();
-        }}
-      >
+        }}>
         APASA
-      </button>
-      {console.log("repo: ", repos)}
+      </button> */}
+      {console.log('repo: ', repos)}
 
       <div className={styles.content}>
         <h3 className={styles.foldername}>{repoName}</h3>
         <div className={styles.fileContainer}>
-          {repos !== undefined ? (
+          {repos.length !== 0 ? (
             repos.map((repo) => (
               <div className={styles.fileElem} key={repo.name}>
-                {repo.type === "dir" ? (
-                  <CardActionArea>
+                {repo.type === 'dir' ? (
+                  <CardActionArea
+                    onClick={() => {
+                      fetchRepo(repo.name);
+                    }}>
                     <div className={styles.folder}>
                       <FolderIcon className={styles.icon} />
-                      <a href="#">
-                        <Typography gutterBottom variant="h4" component="h4">
+                      <a href='#'>
+                        <Typography gutterBottom variant='h4' component='h4'>
                           {repo.name}
                         </Typography>
                       </a>
@@ -105,7 +120,7 @@ const repoName = () => {
                   </CardActionArea>
                 ) : (
                   <div>
-                    <Typography gutterBottom variant="h4" component="h4">
+                    <Typography gutterBottom variant='h4' component='h4'>
                       <InsertDriveFileOutlinedIcon className={styles.icon} />
                       {repo.name}
                     </Typography>
--- creative-hubbers/pages/users/[userName]/index.js
+++ creative-hubbers/pages/users/[userName]/index.js
@@ -7,10 +7,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import { Typography } from '@material-ui/core';
 import { Button } from '@material-ui/core';
-import { useRouter } from 'next/router'
-
-
-
+import { useRouter } from 'next/router';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,7 +15,7 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     padding: theme.spacing(2),
-    textAlign: "center",
+    textAlign: 'center',
     color: theme.palette.text.secondary,
   },
   media: {
@@ -29,70 +26,67 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Users({repos}) {
-        const classes = useStyles();
+export default function Users({ repos }) {
+  const classes = useStyles();
 
   return (
-
- <div className={classes.root}>
+    <div className={classes.root}>
       <Grid container spacing={12}>
-        <Grid item xs={3} >
-          <Paper className={classes.paper}>               
-                    <CardMedia
-                        component='img'
-                        className={classes.media}
-                        src='https://images.unsplash.com/photo-1498804103079-a6351b050096?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80'
-                        title="Contemplative Reptile"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h3" component="h3">
-                            
-                        </Typography>
-                        <Typography gutterBottom variant="h5" component="h5">
-                          
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce urna et urna, nulla quis nisi ac. Accumsan ut ut amet consequat sit sed accumsan mi.
-                        </Typography>
-                    </CardContent>
-            </Paper> 
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>
+            <CardMedia
+              component='img'
+              className={classes.media}
+              src='https://images.unsplash.com/photo-1498804103079-a6351b050096?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80'
+              title='Contemplative Reptile'
+            />
+            <CardContent>
+              <Typography gutterBottom variant='h3' component='h3'></Typography>
+              <Typography gutterBottom variant='h5' component='h5'></Typography>
+              <Typography variant='body2' color='textSecondary' component='p'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                urna et urna, nulla quis nisi ac. Accumsan ut ut amet consequat
+                sit sed accumsan mi.
+              </Typography>
+            </CardContent>
+          </Paper>
         </Grid>
         <Grid item xs={8}>
-               <CardContent>
-               <Typography gutterBottom variant="h2" component="h2">
-                     Repositories
-               </Typography>
-               </CardContent>
-                {repos.map(
-                  (repo) =>
-                    <div className={classes.cards}>
-                <CardActionArea>
-                     <CardContent>
-                         <Typography gutterBottom variant="h5" component="h5">
-                            Repo name: {repo.name}
-                         </Typography>
-                         <Typography variant="body2" color="textSecondary" component="p">
-                             Description: {repo.description}
-                         </Typography>
-                         <Typography gutterBottom variant="h6" component="h6">
-                            Language:   {repo.language}
-                         </Typography>
-                     </CardContent>
-                </CardActionArea>     
-                </div>
-
-                )}
-                
+          <CardContent>
+            <Typography gutterBottom variant='h2' component='h2'>
+              Repositories
+            </Typography>
+          </CardContent>
+          {repos.map((repo) => (
+            <div className={classes.cards}>
+              <CardActionArea>
+                <CardContent>
+                  <Typography gutterBottom variant='h5' component='h5'>
+                    Repo name: {repo.name}
+                  </Typography>
+                  <Typography
+                    variant='body2'
+                    color='textSecondary'
+                    component='p'>
+                    Description: {repo.description}
+                  </Typography>
+                  <Typography gutterBottom variant='h6' component='h6'>
+                    Language: {repo.language}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </div>
+          ))}
         </Grid>
-    </Grid>
- </div>
-  );  
+      </Grid>
+    </div>
+  );
 }
 export async function getServerSideProps(context) {
-  const {userName} = context.query;
+  const { userName } = context.query;
   // Fetch data from external API
-  const res = await fetch(`https://api.github.com/users/${userName}/repos`)
-  const repos = await res.json()
+  const res = await fetch(`https://api.github.com/users/${userName}/repos`);
+  const repos = await res.json();
   // Pass data to the page via props
-  return {props: {repos}}
+  return { props: { repos } };
 }
