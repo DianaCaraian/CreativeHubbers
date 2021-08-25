564bef2289e7fdfa91e73f9faf3736c70755ab0e
Diana Caraian <diana.caraian@evozon.com>
2021-08-25 16:14:31 +0300

    Added multiple folders

--- creative-hubbers/pages/users/[userName]/[repoName]/index.js
+++ creative-hubbers/pages/users/[userName]/[repoName]/index.js
@@ -1,35 +1,16 @@
 import React, { useState, useEffect } from "react";
-import styles from "../../../styles/Repo.module.css";
-import { CardContent, Typography } from "@material-ui/core";
+import styles from "../../../../styles/Repo.module.css";
+import { CardContent, Link, Typography } from "@material-ui/core";
 import FolderIcon from "@material-ui/icons/Folder";
 import InsertDriveFileOutlinedIcon from "@material-ui/icons/InsertDriveFileOutlined";
 import { makeStyles } from "@material-ui/core/styles";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import { useRouter } from "next/router";
 import { useDispatch, useSelector } from "react-redux";
-import { getRepos } from "../../../actions";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    padding: "30px",
-  },
-  details: {
-    display: "flex",
-    flexDirection: "column",
-    padding: "30px",
-  },
-  content: {
-    flex: "1 0 auto",
-  },
-  cover: {
-    width: "25%",
-    height: "100%",
-  },
-}));
+import { getRepos } from "../../../../actions";
 
 const repoName = () => {
-  const classes = useStyles();
+  const [repoDetails, setRepoDetails] = useState({});
 
   const router = useRouter();
 
@@ -57,11 +38,33 @@ const repoName = () => {
 
   // fetchRepo();
 
+  useEffect(() => {
+    if (userName !== undefined) {
+      fetch("https://api.github.com/repos/" + userName + "/" + repoName)
+        .then((data) => data.json())
+        .then((res) => {
+          setRepoDetails(res);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [userName]);
+
   const repos = useSelector((state) => state.user.repos);
+
+  console.log("reoute: ", router);
+  var newUrl = "";
 
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>Repository</h2>
+
+      {repoDetails.language !== undefined ? (
+        <div className={styles.language}>
+          Language: <i>{repoDetails.language}</i>
+        </div>
+      ) : (
+        <div></div>
+      )}
 
       <button
         onClick={() => {
@@ -85,7 +88,6 @@ const repoName = () => {
         APASA
       </button>
       {console.log("repo: ", repos)}
-
       <div className={styles.content}>
         <h3 className={styles.foldername}>{repoName}</h3>
         <div className={styles.fileContainer}>
@@ -93,21 +95,43 @@ const repoName = () => {
             repos.map((repo) => (
               <div className={styles.fileElem} key={repo.name}>
                 {repo.type === "dir" ? (
-                  <CardActionArea>
-                    <div className={styles.folder}>
-                      <FolderIcon className={styles.icon} />
-                      <a href="#">
-                        <Typography gutterBottom variant="h4" component="h4">
-                          {repo.name}
-                        </Typography>
-                      </a>
-                    </div>
-                  </CardActionArea>
+                  <Link className="btn btn-primary">
+                    <CardActionArea>
+                      <div className={styles.folder}>
+                        <FolderIcon className={styles.icon} />
+
+                        <a
+                          href={
+                            "http://localhost:3000/users/" +
+                            userName +
+                            "/" +
+                            repoName +
+                            "/" +
+                            repo.name
+                          }
+                        >
+                          <Typography gutterBottom variant="h4" component="h4">
+                            {repo.name}
+                          </Typography>
+                        </a>
+                      </div>
+                    </CardActionArea>
+                  </Link>
                 ) : (
                   <div>
-                    <Typography gutterBottom variant="h4" component="h4">
+                    <Typography
+                      gutterBottom
+                      variant="h4"
+                      component="h4"
+                      className={styles.elementRepo}
+                    >
                       <InsertDriveFileOutlinedIcon className={styles.icon} />
-                      {repo.name}
+                      <div
+                        onClick={() => console.log("Apasa pe buton")}
+                        className={styles.fileBtn}
+                      >
+                        {repo.name}
+                      </div>
                     </Typography>
                   </div>
                 )}
